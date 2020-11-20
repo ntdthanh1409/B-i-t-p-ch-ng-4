f5a66d046bc69a83ffe18d91c6a4916521736efc
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-11-20 21:56:10 +0700

    Update Hãy tìm phần tử có giá trị lớn nhất của List và in ra màn hình.py

--- Hãy tìm phần tử có giá trị lớn nhất của List và in ra màn hình.py
+++ Hãy tìm phần tử có giá trị lớn nhất của List và in ra màn hình.py
@@ -1,11 +1,14 @@
 import random
 n=int(input("n="))
 list=[]
-for i in range(n):
-    list.append(random.random())
-max_value=list[0]
-for i in list:
-    if i > max_value:
-        max_value = i
-print(list)
-print("max_value =",max_value)
+if n <= 0:
+    print(list,"Không có phần tử nào lớn nhất")
+elif n > 0:
+    for i in range(n):
+        list.append(random.random())
+    max_value=list[0]
+    for i in list:
+        if i > max_value:
+            max_value = i
+    print(list)
+    print("Phần tử lớn nhất của list:",max_value)
