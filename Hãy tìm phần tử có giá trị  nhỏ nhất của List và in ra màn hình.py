ab6525750204b307cda2ae9b6994bc4c667200aa
ntdthanh1409 <72120867+ntdthanh1409@users.noreply.github.com>
2020-11-21 12:03:18 +0700

    Update Hãy tìm phần tử có giá trị  nhỏ nhất của List và in ra màn hình.py

--- Hãy tìm phần tử có giá trị  nhỏ nhất của List và in ra màn hình.py
+++ Hãy tìm phần tử có giá trị  nhỏ nhất của List và in ra màn hình.py
@@ -1,8 +1,10 @@
 import random
 n=int(input("n="))
 list=[]
-if n <= 0:
+if n == 0:
     print(list,"Không có phần tử nào nhỏ nhất")
+elif n < 0:
+    print("Vui lòng nhập số nguyên dương")
 elif n > 0:
     for i in range(n):
         list.append(random.random())
@@ -11,4 +13,4 @@ elif n > 0:
         if i > min_value:
             min_value = i
     print(list)
-    print("Phần tử nhỏ nhất của list: ",min_value)
+    print("Phần tử nhỏ nhất của list:",min_value)
